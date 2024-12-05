d107684c8e81622e204cc4ce735605fc8897abf6
Hyeonseo <90162269+Hyeonseo1021@users.noreply.github.com>
2024-12-05 11:54:52 +0900

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,41 +4,33 @@ import morgan from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { config } from "dotenv";
-
 import userRoutes from "./routes/UserRoutes.js";
 import chatRoutes from "./routes/ChatRoutes.js";
 import Routes from "./routes/Routes.js";
-import voiceRoutes from "./routes/VoiceRoutes.js";  // VoiceRoutes 추가
-
+import voiceRoutes from "./routes/VoiceRoutes.js"; // VoiceRoutes 추가
+import AIRoutes from "./routes/AIRoutes.js";
 config();
-
 const app = express();
-
 // Middlewares
 app.use(cors({ origin: process.env.ORIGIN_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev"));
-
 // Routes
 app.use("/api/user/", userRoutes);
 app.use("/api/chat/", chatRoutes);
 app.use("/api/", Routes);
-app.use("/api", voiceRoutes);  // VoiceRoutes를 "/api" 경로에 추가
-
+app.use("/api", voiceRoutes); // VoiceRoutes를 "/api" 경로에 추가
+app.use("/api/", AIRoutes);
 console.log("Routes added");
-
 // Connections and Listeners
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.z3bws8s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-  )
-  .then(() => {
+    .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.z3bws8s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {
     app.listen(process.env.PORT || 5000);
-    console.log(
-      `Server started on port ${process.env.PORT || 5000} and MongoDB is connected`
-    );
-  })
-  .catch((err) => {
+    console.log(`Server started on port ${process.env.PORT || 5000} and MongoDB is connected`);
+})
+    .catch((err) => {
     console.log(err);
-  });
+});
+//# sourceMappingURL=server.js.map
