042dd23c62b3c3a6099d67bd56c38adfa3d8df5f
Flynn <150012248+saickersj123@users.noreply.github.com>
2025-02-16 00:16:31 +0900

    tsconfig & remove package.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,39 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import commonjs from 'vite-plugin-commonjs';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        additionalData: `@charset "UTF-8";`,
+      },
+    },
+  },
   server: {
     port: 3000,
   },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output:{
+          manualChunks(id) {
+              if (id.includes('node_modules')) {
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+          }
+      }
+  }
   },
 });
